e59078ab0648379c6f2bb1fe63d9edb5860ede5b
ZACK <150016362+zackml@users.noreply.github.com>
2026-04-28 23:27:41 +0400

    fix: remove tsconfigPaths)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-  ],
+  plugins: [react()],
 });
